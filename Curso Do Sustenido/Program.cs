356c30b4511cae70e5a8f0113acde573f4684d53
MARCOS SILVA <marcos.silva312@fatec.sp.gov.br>
2023-02-21 00:20:15 -0300

    matrices

--- Curso Do Sustenido/Program.cs
+++ Curso Do Sustenido/Program.cs
@@ -10,7 +10,7 @@ namespace Curso_Do_Sustenido
     {
         static void Main(string[] args)
         {
-            ExerciciosVetor.exercise9();
+            matrices.matrice();
         }
     }
 }
---
+++ Curso Do Sustenido/matrices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_Do_Sustenido
+{
+    class matrices { 
+    
+        public static void matrice() { 
+            int M, N;
+            int[,] A;
+
+            string[] s1 = Console.ReadLine().Split(' ');
+            M = int.Parse(s1[0]);
+            N = int.Parse(s1[1]);
+            A = new int[M, N];
+
+            for(int i=0; i<M; 
+                i++)
+            {
+                string[] v = Console.ReadLine().Split(' ');
+
+                for(int j=0; j<N; j++)
+                {
+                    A[i, j] = int.Parse(v[j]);
+                 }
+            }
+
+            for(int i=0; i<M; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
